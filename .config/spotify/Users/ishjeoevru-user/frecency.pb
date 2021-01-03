aeeee4177f9b2053cc6571f93b6dcd2aa83e9cf5
alex <alex@localhost.localdomain>
2021-01-03 11:29:00 -0600

    first

---
+++ .config/spotify/Users/ishjeoevru-user/frecency.pb
@@ -0,0 +1,9 @@
+
+>
+'spotify:playlist:097QnXoobZghpONCpH9G2Y	ÎžKú$Üw@ ®±Èÿ
+>
+'spotify:playlist:62NDKZgnNSKb4PTicRm6y1	:ðÇ½w@ ý­Èÿ
+N
+7spotify:user:ishjeoevru:playlist:2lSpDjpwLyUkOYIm4FX0MI	Jè
+ZÂáw@
+ Ë–Åÿ
---
+++ .screenlayout/final.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-1 --off --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 1920x840 --rotate normal --output DP-3 --mode 1920x1080 --pos 3840x0 --rotate right --output HDMI-1 --mode 1920x1080 --pos 0x840 --rotate normal
